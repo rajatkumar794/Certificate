4e7b6c572b984e003d9e236a2f6c784265ad9ccb
Rajat Kumar <rajatrkpuram@gmail.com>
2020-09-30 21:55:47 +0530

    make it better

--- Procfile
+++ Procfile
@@ -1,3 +1,1 @@
-web gunicorn certificate.wsgi:application --log-file - --log-level debug
-python manage.py collectstatic --noinput
-manage.py migrate
+web: gunicorn certificate.wsgi --log-file -
--- certificate/settings.py
+++ certificate/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'whitenoise.storage.CompressedManifestStaticFilesStorage',
 ]
 
 ROOT_URLCONF = 'certificate.urls'
