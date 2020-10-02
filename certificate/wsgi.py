3a4a6da27a062a028432520114576720cf5cfc5d
Rajat Kumar <rajatrkpuram@gmail.com>
2020-10-02 17:20:27 +0530

    updated

--- certificate/wsgi.py
+++ certificate/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'certi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'certificate.settings')
 
 application = get_wsgi_application()
